8fdc1f0b83939865e4d97f36a3502ab05e1dbdab
wllader <wllader.game@seznam.cz>
2025-06-21 11:43:32 +0200

    MidDay 9 - StateAnim skeleton: Added main file, game controller and game entities basic implementation

--- improv0306/D9/StateAnim/game_entities.py
+++ improv0306/D9/StateAnim/game_entities.py
@@ -1,7 +1,67 @@
 import pygame as pg, numpy as np
+from game_controller import GameController
 
-class Entity:
-    pass
+class Entity(pg.sprite.Sprite):
+    def __init__(self, screen:pg.Surface, size:np.ndarray, init_pos:np.ndarray):
+        super().__init__()
+
+        self.screen = screen
+        self._image = pg.Surface(size)
+        self._rect = self._image.get_rect(topleft=init_pos)
+        self.gravity = 10
+
+        self.gc = GameController()
+
+    @property
+    def image(self):
+        self._image.fill((160, 100, 20))
+        return self._image
+    
+    @property
+    def rect(self):
+        return self._rect
+    
+    @property
+    def dt(self):
+        return self.gc.dt
+    
+    def window_correction(self):
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > (h := self.screen.get_height()):
+            self.rect.bottom = h
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > (w := self.screen.get_width()):
+            self.rect.right = w    
+
 
 class Dino(Entity):
-    pass
+    def __init__(self, screen, size, init_pos, speed:float=5., jmp_pwr:float=150):
+        super().__init__(screen, size, init_pos)
+
+        self.speed = speed
+        self.jmp_pwr = jmp_pwr
+
+    
+    @property
+    def grounded(self):
+        return self._rect.bottom == self.screen.get_height()
+
+    
+    def update(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self._rect.x -= self.speed * self.dt
+        if keys[pg.K_d]:
+            self._rect.x += self.speed * self.dt
+
+        if keys[pg.K_w] and self.grounded:
+            self._rect.y -= self.jmp_pwr * self.dt
+
+        # Gravity
+        self._rect.y += self.gravity * self.dt
+
+
+        self.window_correction()
--- improv0306/D9/StateAnim/main.py
+++ improv0306/D9/StateAnim/main.py
@@ -1,12 +1,46 @@
 import pygame as pg, numpy as np
+from game_controller import GameController
+from game_entities import Dino
 
 pg.init()
 
-...
+SIZE = W, H = np.array([600, 600])
+screen = pg.display.set_mode(SIZE)
+pg.display.set_caption("Dinos!")
 
+GREY = np.ones(3, dtype=np.uint8)
+BLACK = GREY * 0
+WHITE = GREY * 255
+
+
+d = Dino(
+    screen,
+    np.array([15, 15]) * 3,
+    np.array([50, H-50])
+)
+
+g_dinos = pg.sprite.Group(
+    d
+)
+
+
+gc = GameController()
 
 running = True
 while running:
-    ...
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # Update
+    g_dinos.update()
+
+    # Draw
+    screen.fill(GREY * 28)
+
+    g_dinos.draw(screen)
+
+    pg.display.flip()
+    gc.tick(144, 10)
 
 pg.quit()
