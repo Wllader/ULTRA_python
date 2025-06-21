775008b87e5125a3fdbcc5c186b5acfead9f3e23
wllader <wllader.game@seznam.cz>
2025-06-21 10:44:02 +0200

    MidDay 9 - Pong: Font rendering, score, polish

---
+++ improv0306/D9/Pong/game_controller.py
@@ -0,0 +1,25 @@
+from numpy import array
+from pygame.time import Clock
+
+class GameController:
+    def __init__(self):
+        self.clock = Clock()
+        self.dt = 0
+        self.scores = array([0, 0])
+
+    def __new__(self):
+        if not hasattr(self, "instance"):
+            self.instance = super(GameController, self).__new__(self)
+
+        return self.instance
+    
+    def tick(self, fps:int, factor:int = 1):
+        self.dt = self.clock.tick(fps) / factor
+
+    def score(self, player:int):
+        self.scores[player] += 1
+
+    def get_score(self, player:int|None = None):
+        if player is None:
+            return self.scores
+        return self.scores[player]
---
+++ improv0306/D9/Pong/pong.py
@@ -0,0 +1,134 @@
+import pygame as pg, numpy as np
+from pong_entities import PongPlayer, PongBot, PongBall, PongBotAdvanced
+from game_controller import GameController
+from sprite_sheet import SpriteSheet
+
+# Initialize
+pg.init()
+
+# Screen dimensions
+SIZE = W, H = np.array([800, 600])
+CENTER = SIZE / 2
+screen = pg.display.set_mode(SIZE)
+pg.display.set_caption("Pong!")
+
+bg = pg.image.load("Sprites/Bg.png")
+bg = pg.transform.scale_by(bg, .78125)
+
+# Colors
+GREY = np.ones(3, dtype=np.uint8)
+WHITE = GREY * 255
+BLACK = GREY * 0
+
+# Game objects
+PADDLE_DIMS = PW, PH = np.array([10, 100])
+BALL_SIZE = np.array([16, 16])
+font = pg.font.Font("freesansbold.ttf", 32)
+
+
+
+paddle_speed = np.array([0, 5])
+ball_speed = np.array([5, 7])
+
+
+p1 = PongBotAdvanced(
+    screen,
+    PADDLE_DIMS,
+    np.array([55, CENTER[1]]),
+    paddle_speed,
+    np.array([0, 255, 0]),
+    SpriteSheet(
+        "Sprites/Paddle.png",
+        np.array([5, 50]),
+        2,
+        color_key=BLACK
+    )
+)
+
+p2 = PongBotAdvanced(
+    screen,
+    PADDLE_DIMS,
+    np.array([W - 55, CENTER[1]]),
+    paddle_speed,
+    np.array([255, 0, 0]),
+    SpriteSheet(
+        "Sprites/Paddle.png",
+        np.array([5, 50]),
+        2,
+        1,
+        BLACK
+    )
+)
+
+ball = PongBall(
+    screen,
+    BALL_SIZE,
+    CENTER,
+    ball_speed,
+    WHITE,
+    SpriteSheet(
+        "Sprites/Ball.png",
+        np.array([16, 16]),
+        1,
+        color_key=BLACK
+    )
+)
+
+p1.ball = ball
+p2.ball = ball
+
+g_paddles = pg.sprite.Group(
+    p1, p2
+)
+
+ball.g_bounce = g_paddles.copy()
+ball.sheet.add_animation("shimmer", list(range(4)))
+ball.sheet.set_animation("shimmer", 250)
+
+g_entities = pg.sprite.Group(
+    p1, ball, p2
+)
+
+gc = GameController()
+
+#Score
+def score_counter():
+    score_text = f"{gc.get_score(0)}   {gc.get_score(1)}"
+    score = font.render(score_text, True, WHITE)
+    score_rect = score.get_rect()
+    score_rect.center = CENTER
+
+    return score, score_rect
+
+
+#! Game loop
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+
+    # Update game state
+    g_entities.update()
+
+
+    # Draw
+    screen.fill(GREY * 18)
+    screen.blit(bg, (0, 0))
+
+    pg.draw.line(screen, WHITE, (CENTER[0], 0), (CENTER[0], H))
+    pg.draw.line(screen, (0, 255, 0), (0,0), (0, H))
+    pg.draw.line(screen, (255, 0, 0), (W-1,0), (W-1, H))
+    pg.draw.line(screen, WHITE, (0,0), (W, 0))
+    pg.draw.line(screen, WHITE, (0,H-1), (W, H-1))
+    screen.blit(*score_counter())
+
+    g_entities.draw(screen)
+
+
+    pg.display.flip()
+    gc.tick(144, 10)
+
+
+pg.quit()
