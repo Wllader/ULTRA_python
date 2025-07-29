8e2e28d37fb7c4a949f4055afd7c707c68a0fbf4
wllader <wllader.game@seznam.cz>
2025-07-29 21:59:05 +0200

    D6 - Pong: Entities basic implementation, Bot assignment, basic image property showcase

--- improv1507/D6/Pong/pong.py
+++ improv1507/D6/Pong/pong.py
@@ -1,4 +1,5 @@
 import pygame as pg, numpy as np
+from pong_entities import PongPlayer, PongBot, PongBall
 
 pg.init()
 
@@ -11,7 +12,7 @@ WHITE = GREY * 255
 BLACK = GREY * 0
 
 PADDLE_DIMS = PW, PH = np.array([10, 100])
-PADDLE_SPEED = np.array([0, 7])
+PADDLE_SPEED = np.array([0, 5])
 
 BALL_SIZE = np.array([16, 16])
 BALL_SPEED = np.array([5, 7])
@@ -21,19 +22,59 @@ screen = pg.display.set_mode(SIZE)
 pg.display.set_caption("Pong!")
 clock = pg.time.Clock()
 
+player = PongPlayer(
+    screen=screen,
+    size=PADDLE_DIMS,
+    init_center_position=(55, CENTER[1]),
+    speed=PADDLE_SPEED,
+    color=(0, 255, 0)
+)
+
+bot = PongBot(
+    screen=screen,
+    size=PADDLE_DIMS,
+    init_center_position=(W - 55, CENTER[1]),
+    speed=PADDLE_SPEED,
+    color=(255, 0, 0)
+)
+
+ball = PongBall(
+    screen=screen,
+    size=BALL_SIZE,
+    init_center_position=CENTER,
+    speed=BALL_SPEED,
+    color=WHITE
+)
+
+bot.target = ball
+
+
+
+g_paddles = pg.sprite.Group(
+    player, bot
+)
+
+ball.g_bounce = g_paddles
+
+g_entities = pg.sprite.Group(
+    ball, g_paddles
+)
+
 
 #! Game Loop
 running = True
+dt = 0
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
     #Update
-
+    g_entities.update(dt=dt)
 
     #Draw
     screen.fill(GREY * 28)
+    g_entities.draw(screen)
 
 
     pg.display.flip()
--- improv1507/D6/Pong/pong_entities.py
+++ improv1507/D6/Pong/pong_entities.py
@@ -1,19 +1,140 @@
 import pygame as pg, numpy as np
 from enum import Enum, auto
 
+class MovingDirection(Enum):
+    Horizontal = auto()
+    Vertical = auto()
 
 class PongEntity(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,
+                 screen:pg.Surface,
+                 size:tuple,
+                 init_center_position:tuple,
+                 speed:tuple,
+                 color:tuple
+        ):
         super().__init__()
+
+        self.size = np.array(size)
+        self.speed = np.array(speed)
+        self.color = np.array(color)
+
+        self.screen = screen
+        self._image = pg.Surface(size, pg.SRCALPHA)
+        self._image.fill(self.color)
+
+        self.rect = self._image.get_rect(center=np.array(init_center_position))
+
+        self.old_rect = self.rect.copy()
+
+    @property
+    def image(self):
+        return self._image
+    
+    @property
+    def screen_center(self):
+        return np.array([
+            self.screen.get_width(),
+            self.screen.get_height()
+        ]) / 2
+
+    def update(self, dt):
+        super().update()
+
+    def window_correction(self):
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > (h := self.screen.get_height()):
+            self.rect.bottom = h
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > (w := self.screen.get_width()):
+            self.rect.right = w
+
 
 
 class PongPlayer(PongEntity):
-    pass
+    def update(self, dt):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            self.rect.y -= self.speed[1] * dt
+        if keys[pg.K_s]:
+            self.rect.y += self.speed[1] * dt
+
+        self.window_correction()
 
 
 class PongBot(PongEntity):
-    pass
+    def __init__(self, screen, size, init_center_position, speed, color):
+        super().__init__(screen, size, init_center_position, speed, color)
+
+        self.target:PongEntity = None
+
+    def update(self, dt): #todo Drift towards a ball / center if ball is flying away from us
+        if self.target:
+            self.rect.centery = self.target.rect.centery
+
+        self.window_correction()
+
+    def drift_towards(self, center:tuple):
+        pass
+
+    def ball_moving_towards_me(self) -> bool:
+        pass
 
 
 class PongBall(PongEntity):
-    pass
+    def __init__(self, screen, size, init_center_position, speed, color):
+        super().__init__(screen, size, init_center_position, speed, color)
+
+        self._image.fill((0, 0, 0, 0))
+        pg.draw.ellipse(self._image, self.color, self._image.get_rect())
+
+        self.g_bounce = pg.sprite.Group()
+
+
+    def update(self, dt):
+        self.rect.x += self.speed[0] * dt
+        self.speed *= self.handle_collisions(MovingDirection.Horizontal)
+
+        self.rect.y += self.speed[1] * dt
+        self.speed *= self.handle_collisions(MovingDirection.Vertical)
+
+        self.window_correction()
+        self.old_rect = self.rect.copy()
+
+    def handle_collisions(self, direction:MovingDirection) -> np.ndarray:
+        if o := self.rect.collideobjects(list(self.g_bounce)):
+            o:PongEntity
+
+            match direction:
+                case MovingDirection.Horizontal:
+                    if self.rect.left <= o.rect.right and self.old_rect.left >= o.rect.right:
+                        self.rect.left = o.rect.right
+                        return np.array([-1, 1])
+                    elif self.rect.right >= o.rect.left and self.old_rect.right <= o.rect.left:
+                        self.rect.right = o.rect.left
+                        return np.array([-1, 1])
+
+                case MovingDirection.Vertical:
+                    if self.rect.top <= o.rect.bottom and self.old_rect.top >= o.rect.bottom:
+                        self.rect.top = o.rect.bottom
+                        return np.array([1, -1])
+                    elif self.rect.bottom >= o.rect.top and self.old_rect.bottom <= o.rect.top:
+                        self.rect.bottom = o.rect.top
+                        return np.array([1, -1])
+                    
+        return np.ones(2, dtype=int)
+
+
+    def window_correction(self):
+        super().window_correction()
+
+        if self.rect.top <= 0 or self.rect.bottom >= self.screen.get_height():
+            self.speed[1] *= -1
+
+        if self.rect.left <= 0 or self.rect.right >= self.screen.get_width():
+            self.speed[0] *= -1
+            self.rect.center = self.screen_center
+            # todo Score
