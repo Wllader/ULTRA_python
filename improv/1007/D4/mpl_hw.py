913fc7ba0ce848918364ff6caef42b60dbc19673
wllader <wllader.game@seznam.cz>
2025-10-16 18:54:15 +0200

    D5 - D4 hw + Integrator class skeleton

--- improv/1007/D4/mpl_hw.py
+++ improv/1007/D4/mpl_hw.py
@@ -1,4 +1,55 @@
-
+from matplotlib import pyplot as plt
+import numpy as np
+from typing import Callable
 
 #todo A) Subplots, kde každý graf je jiného typu
 #todo B) Vypočítat a Vykreslit (pomocí pyplotu) aproximaci integrálu funkce (ideálně obecné, pokud nepůjde tak f=np.abs(np.sin(x)) )
+
+
+def integral(x0, x1, steps:int=100, f:Callable=lambda x_: np.abs(np.sin(x_))) -> float:
+    x = np.linspace(x0, x1, steps, endpoint=False)
+    dx:float = abs(x[1] - x[0])
+    y = f(x)
+
+    return (y * dx).sum()
+
+def draw_integral(x0, x1, steps_f:int=100, steps_i:int=20, f:Callable=lambda x_: np.abs(np.sin(x_))):
+    x_f = np.linspace(x0, x1, steps_f)
+    x_i = np.linspace(x0, x1, steps_i, endpoint=False)
+    y_i = f(x_i)
+    dx = abs(x_i[1] - x_i[0])
+
+    plt.plot(x_f, f(x_f))
+    plt.fill_between(x_f, f(x_f), color="skyblue", alpha=.5)
+    for i in range(len(x_i)):
+        plt.fill_between(
+            [x_i[i], x_i[i] + dx], [y_i[i], y_i[i]], color="red", alpha=.5
+        )
+
+    plt.show()
+
+
+class Integrator:
+    def __init__(self, f:Callable):
+        ...
+
+    def set_function(self, ...):
+        ...
+
+    def draw(self, ...):
+        ...
+
+    def calculate(self, ...):
+        ...
+
+    def draw_calculate(self, ...):
+        ...
+
+
+if __name__ == "__main__":
+    I = Integrator(np.sin)
+
+    I.draw_calculate(...)
+
+    I.set_function(lambda x: np.abs(np.cos(x))**2)
+    I.draw_calculate(...)
--- improv/1007/D4/mpl_showcase.py
+++ improv/1007/D4/mpl_showcase.py
@@ -26,7 +26,7 @@ import numpy as np
 
 # kategorie = list("ABCD")
 # hodnoty = np.random.randint(10, 50, (4, 4))
-# barvy = ["skyblue", "orange", "green", "red"]
+barvy = ["skyblue", "orange", "green", "red"]
 
 # for i, n in enumerate(hodnoty):
 #     p = axes[i]
@@ -50,11 +50,31 @@ import numpy as np
 
 
 labels = ["Python", "Java", "C++", "Rust"]
-sizes = [45, 20, 5, 30]
+# sizes = [45, 20, 5, 30]
 explode = (0, 0, 0.3, 0)
 
-plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=90, explode=explode)
-plt.title("Pie chart of programming language popularity")
+# plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=90, explode=explode)
+# plt.title("Pie chart of programming language popularity")
 
+# plt.show()
+
+
+fig, axes = plt.subplots(2, 2)
+axes = np.array(axes).reshape(-1)
+
+x = np.linspace(-np.pi/2, 2*np.pi, 50)
+axes[0].plot(x, np.cos(x))
+
+sizes = np.random.rand(4)
+sizes = sizes/sizes.sum()
+axes[1].bar(labels, sizes, color=barvy)
+
+x = np.random.rand(50)
+y = np.random.rand(len(x))
+axes[2].scatter(x, y, c=np.random.rand(len(x)), s=np.random.rand(len(x))*1000, alpha=.5)
+
+axes[3].pie(sizes, explode=explode, labels=labels, autopct="%1.1f%%", startangle=120, colors=barvy)
+
+
+plt.tight_layout()
 plt.show()
-
