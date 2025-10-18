c117776816a6aa4b6079ffdd4880da30ee681893
wllader <wllader.game@seznam.cz>
2025-10-18 13:26:39 +0200

    D6 - Pong init skeleton

---
+++ improv/1007/D6/pong/pong.py
@@ -0,0 +1,9 @@
+import pygame as pg, numpy as np
+
+pg.init()
+
+#Todo Postavit základní PyGame skript
+#? (Inicializace, definice konstant, definice objektů, game loop (Event management, Update, Draw, Tick))
+
+#Todo Přidat objekty z pong_entities.py a zprovoznit je pro jednoduchou hru Pong
+# Pokud to nepůjde, nic se neděje!
---
+++ improv/1007/D6/pong/pong_entities.py
@@ -0,0 +1,7 @@
+
+
+#Todo Vytvořit základní PongEntity
+#Todo Odvodit od ní PongPlayer, PongBot a PongBall objekty a implementovat jejich základní chování
+# Pokročilé chování a další věci budeme probírat v dalších lekcích
+# Stačí logika na úrovni square, kterou jsme dělali v úvodním showcasu PyGame
+#  ale pokud budete chtít, vyhrajte si společně s dokumentací PyGame
