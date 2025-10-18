f0a2a9acd4d40b3fbb5ff6a255cd62551071893c
wllader <wllader.game@seznam.cz>
2025-10-18 10:25:49 +0200

    D6 - Pygame - Square

---
+++ improv/1007/D6/pg_showcase.py
@@ -0,0 +1,52 @@
+import pygame as pg, numpy as np
+from square import Square
+
+
+# Inicializace
+pg.init()
+
+
+# Definice konstant
+GREY = np.ones(3, dtype=np.uint8)
+WHITE = GREY * 255
+BLACK = GREY * 0
+
+SIZE = W, H = np.array([800, 600])
+player_speed = 5
+
+
+# Definice objektů
+screen = pg.display.set_mode(SIZE)
+clock = pg.time.Clock()
+pg.display.set_caption("My first game")
+
+p = Square(screen, (50, 50), (50, 50), speed=(10, 5))
+p2 = Square(screen, (100, 235), (50, 50), color=(255, 0, 0))
+
+
+g_squares = pg.sprite.Group(
+    p, p2
+)
+
+#! Game loop
+running = True
+dt = 0
+while running:
+    # Event-management:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # Update
+    g_squares.update(dt=dt)
+
+
+    # Draw
+    screen.fill(GREY * 56)
+    g_squares.draw(screen)
+
+
+    pg.display.flip()
+    dt = clock.tick(144) / 10
+
+pg.quit()
---
+++ improv/1007/D6/square.py
@@ -0,0 +1,37 @@
+import pygame as pg, numpy as np
+
+
+
+class Entity(pg.sprite.Sprite):
+    def __init__(self, screen:pg.Surface, init_pos:tuple[int], size:tuple[int], color:tuple[int] = (255, 255, 255)):
+        super().__init__()
+
+        self.screen = screen
+        self.color = np.array(color)
+        self.image = pg.Surface(size)
+        self.image.fill(self.color)
+
+        self.rect = self.image.get_rect(topleft=init_pos)
+
+
+    def update(self):
+        super().update()
+
+
+class Square(Entity):
+    def __init__(self, screen, init_pos, size, color = (200, 180, 30), speed:tuple[int]=None):
+        super().__init__(screen, init_pos, size, color)
+
+        self.speed = np.array(speed) if speed else np.array((5, 5))
+    
+    def update(self, dt):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_d]:
+            self.rect.x += self.speed[0] * dt
+        if keys[pg.K_a]:
+            self.rect.x -= self.speed[0] * dt
+
+        if keys[pg.K_w]:
+            self.rect.y -= self.speed[1] * dt
+        if keys[pg.K_s]:
+            self.rect.y += self.speed[1] * dt
