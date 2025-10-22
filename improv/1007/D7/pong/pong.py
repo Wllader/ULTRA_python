7221a76428216601162a30a4542994bf273ed36f
wllader <wllader.game@seznam.cz>
2025-10-22 19:49:02 +0200

    D7 - Pong: Entity, Player, Ball, Bot skeleton

---
+++ improv/1007/D7/pong/pong.py
@@ -0,0 +1,90 @@
+import pygame as pg, numpy as np
+from pong_entities import PongPlayer, PongBall, PongBot
+
+pg.init()
+
+#Constants
+SIZE = W, H = np.array([800, 600])
+CENTER = SIZE // 2
+
+GREY = np.ones(3, dtype=np.uint8)
+WHITE = GREY * 255
+BLACK = GREY * 0
+
+PADDLE_DIMS = PW, PH = np.array([10, 100])
+PADDLE_SPEED = np.array([0, 5])
+
+BALL_SIZE = np.array([16, 16])
+BALL_SPEED = np.array([5, 7])
+
+
+#Object
+screen = pg.display.set_mode(SIZE)
+pg.display.set_caption("Pong!")
+clock = pg.time.Clock()
+
+player1 = PongPlayer(
+    screen,
+    PADDLE_DIMS,
+    (55, CENTER[1]),
+    PADDLE_SPEED,
+    (0, 255, 0)
+)
+
+bot = PongBot(
+    screen,
+    PADDLE_DIMS,
+    (W-55, CENTER[1]),
+    PADDLE_SPEED,
+    (255, 0, 0)
+)
+
+ball = PongBall(
+    screen,
+    BALL_SIZE,
+    CENTER,
+    BALL_SPEED,
+    WHITE
+)
+
+ball.g_bounce.add(player1, bot)
+bot.ball = ball
+
+
+
+
+g_entites = pg.sprite.Group(
+    player1, bot, ball
+)
+
+
+#! Game loop
+running = True
+dt = 0
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+
+    # Update
+    g_entites.update(dt=dt)
+
+
+    # Draw
+    screen.fill(GREY * 28)
+    g_entites.draw(screen)
+
+
+    # Tick
+    pg.display.flip()
+    dt = clock.tick(144) / 10
+
+pg.quit()
+
+
+#Todo Postavit základní PyGame skript
+#? (Inicializace, definice konstant, definice objektů, game loop (Event management, Update, Draw, Tick))
+
+#Todo Přidat objekty z pong_entities.py a zprovoznit je pro jednoduchou hru Pong
+# Pokud to nepůjde, nic se neděje!
---
+++ improv/1007/D7/pong/pong_entities.py
@@ -0,0 +1,154 @@
+import pygame as pg, numpy as np
+from enum import Enum, auto
+
+#Todo Vytvořit základní PongEntity
+#Todo Odvodit od ní PongPlayer, PongBot a PongBall objekty a implementovat jejich základní chování
+# Pokročilé chování a další věci budeme probírat v dalších lekcích
+# Stačí logika na úrovni square, kterou jsme dělali v úvodním showcasu PyGame
+#  ale pokud budete chtít, vyhrajte si společně s dokumentací PyGame
+
+class MovingDirection(Enum):
+    Horizontal = auto()
+    Vertical = auto()
+
+
+class PongEntity(pg.sprite.Sprite):
+    def __init__(self, screen:pg.Surface, size:tuple[int], init_center_pos:tuple[int], speed:tuple[int], color:tuple[int]):
+        super().__init__()
+
+        self.speed = np.array(speed)
+        self.color = np.array(color, dtype=np.uint8)
+
+        self.screen = screen
+        self._image = pg.Surface(size, pg.SRCALPHA)
+        self._image.fill(self.color)
+
+        self.rect = self._image.get_rect(center=np.array(init_center_pos))
+        self.old_rect = self.rect.copy()
+
+    @property
+    def image(self):
+        return self._image
+    
+    @property
+    def screen_center(self):
+        return np.array([
+            self.screen.get_width(),
+            self.screen.get_height()
+        ]) // 2
+    
+    def update(self, dt):
+        super().update()
+
+    def window_correction(self):
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > (h := self.screen.get_height()):
+            self.rect.bottom = h
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > (w := self.screen.get_width()):
+            self.rect.right = w
+
+
+
+class PongPlayer(PongEntity):
+    def update(self, dt):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w] or keys[pg.K_UP]:
+            self.rect.y -= self.speed[1] * dt
+
+        if keys[pg.K_s] or keys[pg.K_DOWN]:
+            self.rect.y += self.speed[1] * dt
+
+        self.window_correction()
+        self.old_rect = self.rect.copy()
+
+
+class PongBot(PongEntity):
+    def __init__(self, screen, size, init_center_pos, speed, color):
+        super().__init__(screen, size, init_center_pos, speed, color)
+
+        self.ball:PongEntity = None
+
+    def update(self, dt):
+        if self.ball:
+            self.rect.centery = self.ball.rect.centery
+
+        self.window_correction()
+
+    def drift_towards(self, center:tuple[int]): #todo Implement drifting towards given coordinates
+        pass
+
+    def ball_moving_towards_me(self) -> bool: #todo Implement
+        pass
+
+
+class PongBall(PongEntity):
+    def __init__(self, screen, size, init_center_pos, speed, color):
+        super().__init__(screen, size, init_center_pos, speed, color)
+
+        self._image.fill((0, 0, 0, 0))
+        pg.draw.ellipse(self._image, self.color, self._image.get_rect())
+
+        self.g_bounce = pg.sprite.Group()
+
+    @property
+    def image(self):
+        pg.draw.ellipse(self._image, self.color, self._image.get_rect())
+        return self._image
+
+    def update(self, dt):
+        self.rect.x += self.speed[0] * dt
+        self.speed *= self.handle_collisions(MovingDirection.Horizontal)
+
+        self.rect.y += self.speed[1] * dt
+        self.speed *= self.handle_collisions(MovingDirection.Vertical)
+
+        self.window_correction()
+        self.old_rect = self.rect.copy()
+
+    def handle_collisions(self, direction:MovingDirection):
+        if o := self.rect.collideobjects(list(self.g_bounce)):
+            o:PongEntity
+
+            self.color = o.color
+
+            match direction:
+                case MovingDirection.Horizontal:
+                    if self.rect.left <= o.rect.right and self.old_rect.left >= o.rect.right:
+                        self.rect.left = o.rect.right
+                        return np.array([-1, 1])
+                    elif self.rect.right >= o.rect.left and self.old_rect.right <= o.rect.left:
+                        self.rect.right = o.rect.left
+                        return np.array([-1, 1])
+
+                case MovingDirection.Vertical:
+                    if self.rect.top <= o.rect.bottom and self.old_rect.top > o.rect.bottom:
+                        self.rect.top = o.rect.bottom
+                        return np.array([1, -1])
+                    elif self.rect.bottom >= o.rect.top and self.old_rect.bottom < o.rect.top:
+                        self.rect.bottom = o.rect.top
+                        return np.array([1, -1])
+
+        return 1
+
+
+    def window_correction(self):
+        super().window_correction()
+
+        if self.rect.top <= 0 or self.rect.bottom >= self.screen.get_height():
+            self.speed[1] *= -1
+
+        if self.rect.left <= 0 or self.rect.right >= self.screen.get_width():
+            self.speed[0] *= -1
+            shoh = self.rect.height // 2
+            self.rect.center = np.array([
+                self.screen_center[0],
+                np.random.randint(shoh, self.screen.get_height() - shoh)
+            ])
+
+    
+
+    
