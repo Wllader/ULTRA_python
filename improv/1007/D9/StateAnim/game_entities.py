c044431a72ab29cb17b1960414167ecf6c2aa457
wllader <wllader.game@seznam.cz>
2025-10-25 12:24:11 +0200

    D9 - StateAnim skeleton

---
+++ improv/1007/D9/StateAnim/game_entities.py
@@ -0,0 +1,11 @@
+import pygame as pg, numpy as np
+from game_controller import GameController
+
+
+class Entity(pg.sprite.Sprite):
+    ...
+
+
+
+class Dino(Entity):
+    ...
---
+++ improv/1007/D9/StateAnim/main.py
@@ -0,0 +1,6 @@
+import pygame as pg, numpy as np
+from game_controller import GameController
+
+pg.init()
+
+...
