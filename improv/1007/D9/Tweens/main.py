65878a8ab99020a0db82c1a2ec353c8be0d77feb
wllader <wllader.game@seznam.cz>
2025-10-25 15:40:17 +0200

    D9 - Tweens

---
+++ improv/1007/D9/Tweens/main.py
@@ -0,0 +1,68 @@
+import pygame as pg, numpy as np
+from game_controller import GameController
+from square import Square, ClickState
+
+pg.init()
+
+SIZE = W, H = np.array([600, 600])
+CENTER = SIZE / 2
+
+screen = pg.display.set_mode(SIZE)
+pg.display.set_caption("Interpolation demo")
+
+GREY = np.ones(3, dtype=np.uint8)
+WHITE = GREY * 255
+BLACK = GREY * 0
+
+
+mk_held = np.zeros(3, dtype=bool)
+def click(button:int) -> ClickState:
+    p = pg.mouse.get_pressed()[button]
+    h = mk_held[button]
+
+    if p and not h:
+        mk_held[button] = True
+        return ClickState.CLICKED
+    elif p and h:
+        return ClickState.HELD
+    elif not p and h:
+        mk_held[button] = False
+        return ClickState.RELEASED
+    # elif p and not h and not c:
+    #     return ClickState.MISSED
+
+g_squares = pg.sprite.Group()
+
+
+gc = GameController()
+while gc.running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            gc.stop()
+
+    #Update
+    g_squares.update()
+    if click(0) == ClickState.CLICKED:
+        g_squares.add(
+            Square(
+                screen,
+                pg.mouse.get_pos()
+            )
+        )
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_SPACE]:
+        for s in g_squares:
+            s:Square
+            s.change_pos(pg.mouse.get_pos())
+
+    #Draw
+    screen.fill(GREY * 28)
+    g_squares.draw(screen)
+
+
+
+    pg.display.flip()
+    gc.tick(144)
+
+pg.quit()
