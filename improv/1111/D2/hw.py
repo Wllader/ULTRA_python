fdd4b1a0c68f0463ac07468b048177b1f1694c99
wllader <wllader.game@seznam.cz>
2025-11-13 21:52:56 +0100

    D2 - Done

---
+++ improv/1111/D2/hw.py
@@ -0,0 +1,7 @@
+
+
+def _min(l:list[float]) -> float:
+    pass
+
+def _druhe_nejmensi(l:list[float]) -> float:
+    pass
--- improv/1111/D2/triangle.py
+++ improv/1111/D2/triangle.py
@@ -32,35 +32,83 @@ class Triangle:
 
     # Privátní API
     def _triangel_ra(self) -> str:
+        l:list[str] = []
         for i in range(1, self.vyska+1):
-            print(i*"*")
+            # print(i*"*")
+            # s += i*"*" + "\n"
+            l.append(i*"*")
+
+        return "\n".join(l)
 
     def _triangle_el_w(self, sirka:int) -> str:
+        l:list[str] = []
         for i in range(1, sirka+1, 2):
             pocet_mezer = (sirka-i)//2
-
-            print(
-                pocet_mezer*" " + i*"*" + pocet_mezer*" "
-            )
+            # print(pocet_mezer*" " + i*"*" + pocet_mezer*" ")
+            l.append(f"{pocet_mezer*" "}{i*"*"}{pocet_mezer*" "}")
+        
+        return "\n".join(l)
 
     def _triangle_el(self) -> str:
         sirka = 1 + 2*(self.vyska-1)
-        self._triangle_el_w(sirka)
+        return self._triangle_el_w(sirka)
 
 
     # Veřejné API
     def pravouhly(self):
-        self._triangel_ra()
+        print(self._triangel_ra())
         print()
 
     def rovnoramenny(self):
-        self._triangle_el()
+        print(self._triangle_el())
         print()
+
+    def get_pravouhly(self) -> str:
+        return self._triangel_ra()
+    
+    def get_rovnoramenny(self) -> str:
+        return self._triangle_el()
 
 
 if __name__ == "__main__":
     t = Triangle(6)
 
-    t.pravouhly()
-    t.rovnoramenny()
+    # t.pravouhly()
+    # t.rovnoramenny()
+
+    # print(
+    #     t.get_pravouhly(),
+    #     t.get_rovnoramenny(),
+
+    #     sep="\n---\n"
+    # )
+
+    # f = open("trojuhelnik.txt", "w")
+    # f.write(t.get_pravouhly())
+    # f.write("\n---\n")
+    # f.write(t.get_rovnoramenny())
+
+    # f.close()
+
+    # try:
+    #     print("Try")
+    #     f = open("test.txt", "w")
+    #     f.write(str(5/0))
+
+    # # except Exception as e:
+    # #     print("Except")
+    # #     print(e)
+
+    # finally:
+    #     print("Finally")
+    #     f.close()
+
+    # print("Past try_block")
+
+
+    with open("test.txt", "w") as f:
+        f.write(str(5/3))
+        f.write(str(5/0))
+
+
 
