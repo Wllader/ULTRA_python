68e332843ce55739c986770c6517a7db4830d25b
wllader <wllader.game@seznam.cz>
2025-11-27 19:22:04 +0100

    D7 - Pong skeleton assignment

---
+++ improv/1111/D7/pong/pong.py
@@ -0,0 +1,7 @@
+import pygame as pg, numpy as np
+
+#Todo Postavit základní PyGame skript
+#? (Inicializace, definice konstant, definice objektů, game loop (Event management, Update, Draw, Tick+Flip))
+
+#Todo Přidat objekty z pong_entities.py a zprovoznit je pro jednoduchou hru Pong
+# Pokud tohle nepůjde, nic se neděje! 
---
+++ improv/1111/D7/pong/pong_entities.py
@@ -0,0 +1,4 @@
+import pygame as pg, numpy as np
+
+#Todo vytvořit základní PongEntity
+#Todo Odvodit od ní PongPlayer, PongBot a PongBall objekty a implementovat jejich základní chování
