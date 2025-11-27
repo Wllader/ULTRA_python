8ff69e8c47639abcdb8a389661216bbb621e4621
wllader <wllader.game@seznam.cz>
2025-11-27 21:27:46 +0100

    D7 - Pong: Collisions and basic bot skeleton

--- improv/1111/D7/pong/pong.py
+++ improv/1111/D7/pong/pong.py
@@ -1,4 +1,94 @@
 import pygame as pg, numpy as np
+from pong_entities import PongPlayer, PongBot, PongBall
+
+
+
+pg.init()
+
+# Constants
+SIZE = W, H = np.array([800, 600])
+CENTER = SIZE / 2
+
+GREY = np.ones(3, dtype=np.uint8)
+WHITE = GREY * 255
+BLACK = GREY * 0
+
+PADDLE_DIMS = PW, PH = np.array([10, 100])
+PADDLE_SPEED = np.array([0, 5])
+
+BALL_SIZE = np.array([16, 16])
+BALL_SPEED = np.array([5, 7])
+
+
+#Objects
+screen = pg.display.set_mode(SIZE)
+pg.display.set_caption("Pong!")
+clock = pg.time.Clock()
+
+player = PongPlayer(
+    screen,
+    PADDLE_DIMS,
+    (50, CENTER[1]),
+    PADDLE_SPEED,
+    (0, 255, 0)
+)
+
+bot = PongBot(
+    screen,
+    PADDLE_DIMS,
+    (W-50, CENTER[1]),
+    PADDLE_SPEED,
+    (255, 0, 0)
+)
+
+ball = PongBall(
+    screen,
+    BALL_SIZE,
+    CENTER,
+    BALL_SPEED,
+    WHITE
+)
+
+bot.ball = ball
+
+
+g_paddles = pg.sprite.Group(
+    player, bot
+)
+
+ball.g_bounce.add(g_paddles)
+
+
+g_entities = pg.sprite.Group(
+    g_paddles, ball
+)
+
+
+#! Game loop
+running = True
+dt = 0
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    
+    #Update
+    g_entities.update(dt=dt)
+
+    #Draw
+    screen.fill(GREY * 28)
+    g_entities.draw(screen)
+
+
+
+    pg.display.flip()
+    dt = clock.tick(144) / 10
+
+
+pg.quit()
+
+
 
 #Todo Postavit základní PyGame skript
 #? (Inicializace, definice konstant, definice objektů, game loop (Event management, Update, Draw, Tick+Flip))
--- improv/1111/D7/pong/pong_entities.py
+++ improv/1111/D7/pong/pong_entities.py
@@ -2,3 +2,107 @@ import pygame as pg, numpy as np
 
 #Todo vytvořit základní PongEntity
 #Todo Odvodit od ní PongPlayer, PongBot a PongBall objekty a implementovat jejich základní chování
+
+
+class PongEntity(pg.sprite.Sprite):
+    def __init__(self, screen:pg.Surface, size:tuple[int], init_center_pos:tuple[int], speed:tuple[int], color:tuple[int]):
+        super().__init__()
+
+        self.size = np.array(size)
+        self.speed = np.array(speed)
+        self.color = np.array(color)
+
+        self.screen = screen
+        self._image = pg.Surface(size, pg.SRCALPHA)
+        self._image.fill(self.color)
+
+        self.rect = self._image.get_rect(center=np.array(init_center_pos))
+        self.old_rect = self.rect.copy()
+
+    @property
+    def image(self):
+        return self._image
+    
+
+    @property
+    def screen_center(self):
+        return np.array([
+            self.screen.get_width(),
+            self.screen.get_height()
+        ]) / 2
+    
+
+    def update(self, dt):
+        super().update()
+
+    def window_correction(self):
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > (h := self.screen.get_height()):
+            self.rect.bottom = h
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > (w := self.screen.get_width()):
+            self.rect.right = w
+
+class PongPlayer(PongEntity):
+    def update(self, dt):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w] or keys[pg.K_UP]:
+            self.rect.y -= self.speed[1] * dt
+        if keys[pg.K_s] or keys[pg.K_DOWN]:
+            self.rect.y += self.speed[1] * dt
+
+        self.window_correction()
+        self.old_rect = self.rect.copy()
+
+
+class PongBot(PongEntity):
+    def __init__(self, screen, size, init_center_pos, speed, color):
+        super().__init__(screen, size, init_center_pos, speed, color)
+
+        self.ball:PongEntity = None
+
+    def update(self, dt): #todo logic using unimplemented methods
+        if self.ball:
+            self.rect.centery = self.ball.rect.centery
+
+        self.window_correction()
+
+    def drift_towards(self, center:tuple): #todo Implement drifiting towards given coordinates (with our speed)
+        pass
+
+    
+    def ball_moving_towards_me(self) -> bool: #todo Implement
+        pass
+
+
+class PongBall(PongEntity):
+    def __init__(self, screen, size, init_center_pos, speed, color):
+        super().__init__(screen, size, init_center_pos, speed, color)
+
+        self._image.fill((0, 0, 0, 0))
+        pg.draw.ellipse(self._image, self.color, self._image.get_rect())
+
+        self.g_bounce = pg.sprite.Group()
+
+    def update(self, dt):
+        self.rect.x += self.speed[0] * dt
+        self.rect.y += self.speed[1] * dt
+
+        self.window_correction()
+        self.old_rect = self.rect.copy()
+
+    def window_correction(self):
+        super().window_correction()
+
+        if self.rect.top <= 0 or self.rect.bottom >= self.screen.get_height():
+            self.speed[1] *= -1
+
+        if self.rect.left <= 0 or self.rect.right >= self.screen.get_width():
+            self.speed[0] *= -1
+            self.rect.center = self.screen_center
+            #todo Score
+
+
