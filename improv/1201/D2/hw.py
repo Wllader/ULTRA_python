a394141e424d1184b1d0a121ad648cc590eac3fb
wllader <wllader.game@seznam.cz>
2025-12-03 21:30:15 +0100

    D2 - Triangles done, Triangle class & min hw skeletons

---
+++ improv/1201/D2/hw.py
@@ -0,0 +1,10 @@
+
+
+# [1, 5, 8, -3, 2] -> -3
+def _min(l:list[float]) -> float:
+    pass
+
+
+# [1, 5, 8, -3, 2] -> 1
+def _druhe_min(l:list[float]) -> float:
+    pass
--- improv/1201/D2/triangle.py
+++ improv/1201/D2/triangle.py
@@ -1,8 +1,44 @@
 
-
-
 def triangle_ra(h:int):
-    pass
+    for i in range(1, h+1):
+        print(i*"*")
 
 def triangle_el(h:int):
-    pass
+    w = 1 + (h-1)*2
+    triangle_el_w(w)
+
+
+def triangle_el_w(w:int):
+    for i in range(1, w+1, 2):
+        pocet_mezer = (w-i) // 2
+
+        print(pocet_mezer*" " + i*"*" + pocet_mezer*" ")
+
+
+class Triangle: #todo implement
+    def __init__(self, h:int):
+        pass
+
+    # Funkce...
+    def get_pravouhly(self) -> str:
+        pass
+
+    def get_rovnoramenny(self) -> str:
+        pass
+
+    def pravouhly(self):
+        pass
+
+    def rovnoramenny(self):
+        pass
+
+
+
+
+# Useful hints:
+def foo(n:int) -> str:
+    return str(n)
+
+
+def goo(l:list[str]) -> str:
+    return "__".join(l)
